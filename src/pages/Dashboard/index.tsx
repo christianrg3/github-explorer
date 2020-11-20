c3970f249bb941576cde208ed3b002cff0aa7886
Christian Gonçalves <christiangoncalvesr@gmail.com>
2020-11-19 23:27:17 -0300

    Added Dashboard styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github explorer logo" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/42493725?s=460&u=d5c1eae8ff504436a6fe57615cb40157ed4f7272&v=4"
+            alt="Github avatar"
+          />
+          <div>
+            <strong>christianrg3/dotfiles</strong>
+            <p>My dotfiles</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
